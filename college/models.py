eb1a3a6f03ab3acb5fb3abba07a27b9c5414723a
Wesley <brooks.wesley1217@gmail.com>
2023-03-06 22:53:36 -0500

    AWS fix
    Fixed

--- college/models.py
+++ college/models.py
@@ -3,8 +3,12 @@ from django.db import models
 # Create your models here.
 
 class College(models.Model):
-    name = models.CharField(max_length=120)
-    collegeAIKey = models.IntegerField(null = True)
+    name = models.CharField(max_length=120, blank=True, default="")
+    collegeAIKey = models.IntegerField(null = True, blank=True, default="")
+    acceptance_rate = models.FloatField(default=0.0, blank=True)
+    sat_score = models.IntegerField(blank=True, default=0)
+    average_fin_aid = models.IntegerField(blank=True, default=0)
+    ten_year_earnings = models.IntegerField(blank=True, default=0)
 
     def __str__(self):
         return self.name
